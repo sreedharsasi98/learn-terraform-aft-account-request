8d6f8fce27e5eed8ea5cf72db04d22589941c7ce
sreedharsasi98 <sreedharsasi102@gmail.com>
2025-02-20 16:42:48 +0530

    customizing sand

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,7 +71,7 @@ module "dev1" {
   }
 
   change_management_parameters = {
-    change_requested_by = "customizing dev account"
+    change_requested_by = "customizing dev account1"
     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
 
--- terraform/variable.tf
+++
@@ -1,4 +0,0 @@
-variable "account_customizations_name" {
-  type = list(string)
-  description = "List of account customization names"
-}
