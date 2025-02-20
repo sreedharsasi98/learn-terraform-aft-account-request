088d963c082dc67c919557711e83525cbf3b8bbd
sreedharsasi98 <sreedharsasi102@gmail.com>
2025-02-20 14:38:44 +0530

    creating dev1 account

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,3 +54,30 @@ module "dev" {
 
   account_customizations_name = "dev"
 }
+module "dev1" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "sreedharsasi102+++++@gmail.com"
+    AccountName               = "dev"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "sreedharsasi102+++++@gmail.com"
+    SSOUserFirstName          = "Sasidharan"
+    SSOUserLastName           = "S"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "dev1"
+  }
+
+  account_customizations_name = "dev"
+}
